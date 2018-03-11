106b1c79354861dc743737b989b57c310f9360a3
Hellwig Geisse <hellwig.geisse@mni.thm.de>
2018-03-11 14:08:22 +0100

    add preliminary version of bootlink

--- link/bootlink/bootlink.c
+++ link/bootlink/bootlink.c
@@ -12,21 +12,23 @@
 #include <termios.h>
 
 
-//#define MAX_RETRIES	5
-//#define MAX_WAIT	1000000
+#define BLOCK_SIZE	512
 
-//#define REQ		((unsigned char) 0x20)
-//#define REC		((unsigned char) 0x21)
-//#define SND		((unsigned char) 0x22)
-//#define ACK		((unsigned char) 0x10)
-//#define NAK		((unsigned char) 0x11)
+#define LINE_SIZE	200
+#define MAX_TOKENS	20
 
+#define REQ		((unsigned char) 0x20)
+#define REC		((unsigned char) 0x21)
+#define SND		((unsigned char) 0x22)
+#define ACK		((unsigned char) 0x10)
+#define NAK		((unsigned char) 0x11)
 
-static int debug = 0;
 
 static int sfd = -1;
 static struct termios origOptions;
 static struct termios currOptions;
+
+static int run;
 
 
 void serialClose(void);
@@ -95,9 +97,385 @@ int serialRcv(unsigned char *bp) {
 }
 
 
+unsigned char rcvByte(void) {
+  unsigned char b;
+
+  while (!serialRcv(&b)) ;
+  return b;
+}
+
+
+int rcvInt(void) {
+  int i;
+  unsigned char b;
+
+  i = 0;
+  while (!serialRcv(&b)) ;
+  i |= (unsigned int) b <<  0;
+  while (!serialRcv(&b)) ;
+  i |= (unsigned int) b <<  8;
+  while (!serialRcv(&b)) ;
+  i |= (unsigned int) b << 16;
+  while (!serialRcv(&b)) ;
+  i |= (unsigned int) b << 24;
+  return i;
+}
+
+
+void sndByte(unsigned char b) {
+  while (!serialSnd(b)) ;
+}
+
+
+void sndInt(unsigned int i) {
+  while (!serialSnd((i >>  0) & 0xFF)) ;
+  while (!serialSnd((i >>  8) & 0xFF)) ;
+  while (!serialSnd((i >> 16) & 0xFF)) ;
+  while (!serialSnd((i >> 24) & 0xFF)) ;
+}
+
+
+void sndStr(char *s) {
+  while (*s != '\0') {
+    sndByte(*s);
+    s++;
+  }
+  sndByte(0);
+}
+
+
+/**************************************************************/
+
+
+void sendBootFile(FILE *bootFile, unsigned int addr) {
+  unsigned char buf[BLOCK_SIZE];
+  int n, i;
+
+  while (1) {
+    n = fread(buf, 1, BLOCK_SIZE, bootFile);
+    if (n < 0) {
+      error("cannot read boot file");
+    }
+    if (n == 0) {
+      break;
+    }
+    sndInt(n);
+    sndInt(addr);
+    for (i = 0; i < n; i++) {
+      sndByte(buf[i]);
+    }
+    addr += n;
+    if (n < BLOCK_SIZE) {
+      break;
+    }
+  }
+  sndInt(0);
+}
+
+
+/**************************************************************/
+
+
+void getAndShowAnswer(void) {
+  int run;
+  unsigned char type;
+  unsigned char b;
+  int n;
+
+  run = 1;
+  while (run) {
+    type = rcvByte();
+    switch (type) {
+      case 1:
+        /* integer */
+        n = rcvInt();
+        printf("%d  ", n);
+        break;
+      case 2:
+        /* hex */
+        n = rcvInt();
+        printf("0x%08X  ", n);
+        break;
+      case 3:
+        /* real */
+        printf("!!! real not yet\n");
+        break;
+      case 4:
+        /* string */
+        while (1) {
+          b = rcvByte();
+          if (b == 0) {
+            break;
+          }
+          printf("%c", b);
+        }
+        printf("  ");
+        break;
+      case 5:
+        /* bit */
+        b = rcvByte();
+        printf("%c", b);
+        break;
+      case 6:
+        /* line */
+        printf("\n");
+        break;
+      case 7:
+        /* end */
+        run = 0;
+        break;
+      default:
+        printf("unknown type byte %u\n", (unsigned int) type);
+    }
+  }
+  printf("\n");
+}
+
+
+/**************************************************************/
+
+
+void ping(int argc, char *argv[]) {
+  unsigned char b;
+
+  sndByte(REQ);
+  b = rcvByte();
+  if (b == ACK) {
+    printf("ACK\n");
+  } else
+  if (b == NAK) {
+    printf("NAK\n");
+  } else {
+    printf("error: unknown answer\n");
+  }
+}
+
+
+void xh2o(int argc, char *argv[]) {
+  char *name;
+  FILE *file;
+  unsigned char b;
+  unsigned char buf[255];
+  int n, i;
+
+  name = argv[1];
+  file = fopen(name, "r");
+  if (file == NULL) {
+    printf("error: cannot open file '%s' for read on host\n", name);
+    return;
+  }
+  sndByte(REC);
+  sndStr(name);
+  b = rcvByte();
+  if (b != ACK) {
+    printf("error: no ACK for filename '%s' from Oberon system\n", name);
+    fclose(file);
+    return;
+  }
+  while (1) {
+    n = fread(buf, 1, 255, file);
+    if (n < 0) {
+      error("cannot read local file");
+    }
+    if (n == 0) {
+      sndByte(0);
+      b = rcvByte();
+      if (b != ACK) {
+        printf("error: no ACK for file data from Oberon system\n");
+        fclose(file);
+        return;
+      }
+    }
+    sndByte(n & 0xFF);
+    for (i = 0; i < n; i++) {
+      sndByte(buf[i]);
+    }
+    b = rcvByte();
+    if (b != ACK) {
+      printf("error: no ACK for file data from Oberon system\n");
+      fclose(file);
+      return;
+    }
+    if (n < 255) {
+      printf("ACK\n");
+      fclose(file);
+      return;
+    }
+  }
+}
+
+
+void xo2h(int argc, char *argv[]) {
+  char *name;
+  FILE *file;
+  unsigned char b;
+  unsigned char buf[255];
+  int n, i;
+
+  name = argv[1];
+  file = fopen(name, "w");
+  if (file == NULL) {
+    printf("error: cannot open file '%s' for write on host\n", name);
+    return;
+  }
+  sndByte(SND);
+  sndStr(name);
+  b = rcvByte();
+  if (b != ACK) {
+    printf("error: no ACK for filename '%s' from Oberon system\n", name);
+    fclose(file);
+    return;
+  }
+  while (1) {
+    n = rcvByte();
+    if (n == 0) {
+      sndByte(ACK);
+      printf("ACK\n");
+      fclose(file);
+      return;
+    }
+    for (i = 0; i < n; i++) {
+      buf[i] = rcvByte();
+    }
+    if (fwrite(buf, 1, n, file) != n) {
+      error("cannot write local file");
+    }
+    sndByte(ACK);
+    if (n < 255) {
+      printf("ACK\n");
+      fclose(file);
+      return;
+    }
+  }
+}
+
+
+void mirr(int argc, char *argv[]) {
+  int arg;
+  char *endp;
+
+  arg = strtol(argv[1], &endp, 10);
+  if (*endp != '\0') {
+    printf("error: cannot read integer argument\n");
+    return;
+  }
+  sndInt(0);
+  sndInt(arg);
+  getAndShowAnswer();
+}
+
+
+void fill(int argc, char *argv[]) {
+  int arg;
+  char *endp;
+
+  arg = strtol(argv[1], &endp, 0);
+  if (*endp != '\0') {
+    printf("error: cannot read integer argument\n");
+    return;
+  }
+  sndInt(2);
+  sndInt(arg);
+  getAndShowAnswer();
+}
+
+
+void watch(int argc, char *argv[]) {
+  sndInt(7);
+  getAndShowAnswer();
+}
+
+
+void shmod(int argc, char *argv[]) {
+  sndInt(10);
+  getAndShowAnswer();
+}
+
+
+void shcmd(int argc, char *argv[]) {
+  sndInt(11);
+  sndStr(argv[1]);
+  getAndShowAnswer();
+}
+
+
+void ldboot(int argc, char *argv[]) {
+  sndInt(100);
+  sndStr(argv[1]);
+  getAndShowAnswer();
+}
+
+
+void clrdir(int argc, char *argv[]) {
+  sndInt(101);
+  getAndShowAnswer();
+}
+
+
+void quit(int argc, char *argv[]) {
+  run = 0;
+}
+
+
+/**************************************************************/
+
+
+typedef struct {
+  char *name;
+  int minArgc;
+  void (*func)(int argc, char *argv[]);
+} Cmd;
+
+
+Cmd cmds[] = {
+  { "ping",   1, ping   },
+  { "xh2o",   2, xh2o   },
+  { "xo2h",   2, xo2h   },
+  { "mirr",   2, mirr   },
+  { "fill",   2, fill   },
+  { "watch",  1, watch  },
+  { "shmod",  1, shmod  },
+  { "shcmd",  2, shcmd  },
+  { "ldboot", 2, ldboot },
+  { "clrdir", 1, clrdir },
+  { "q",      1, quit   },
+};
+
+
+Cmd *lookupCmd(char *name) {
+  int i;
+
+  for (i = 0; i < sizeof(cmds) / sizeof(cmds[0]); i++) {
+    if (strcmp(cmds[i].name, name) == 0) {
+      return &cmds[i];
+    }
+  }
+  return NULL;
+}
+
+
+/**************************************************************/
+
+
 void usage(char *myself) {
-  printf("Usage: %s -p <serial port> -b <boot file>\n", myself);
+  printf("Usage: %s -p <serial port> [-b <boot file>]\n", myself);
   exit(1);
+}
+
+
+int tokenize(char *line, char *tokens[], int maxTokens) {
+  int n;
+  char *p;
+
+  n = 0;
+  p = strtok(line, " \t\n\r");
+  while (p != NULL) {
+    if (n < maxTokens) {
+      tokens[n++] = p;
+    }
+    p = strtok(NULL, " \t\n\r");
+  }
+  return n;
 }
 
 
@@ -107,6 +485,11 @@ int main(int argc, char *argv[]) {
   char *serialPort;
   char *bootName;
   FILE *bootFile;
+  unsigned char b;
+  char line[LINE_SIZE];
+  char *tokens[MAX_TOKENS];
+  int n;
+  Cmd *cmd;
 
   serialPort = NULL;
   bootName = NULL;
@@ -137,10 +520,53 @@ int main(int argc, char *argv[]) {
   if (serialPort == NULL) {
     error("no serial port specified");
   }
-  if (bootName == NULL) {
-    error("no boot file specified");
-  }
   serialOpen(serialPort);
+  while (serialRcv(&b)) ;
+  if (bootName != NULL) {
+    bootFile = fopen(bootName, "r");
+    if (bootFile == NULL) {
+      error("cannot open boot file '%s'", bootName);
+    }
+    sendBootFile(bootFile, 0);
+    fclose(bootFile);
+  }
+  run = 1;
+  while (run) {
+    printf("\n");
+    printf("Commands:\n");
+    printf("ping                request sign of life\n");
+    printf("xh2o <filename>     transfer <filename> from host to Oberon\n");
+    printf("xo2h <filename>     transfer <filename> from Oberon to host\n");
+    printf("mirr <integer>      mirror <integer> back\n");
+    printf("fill <integer>      fill display with <integer>\n");
+    printf("watch               watch\n");
+    printf("shmod               show modules\n");
+    printf("shcmd <modname>     show commands for <modname>\n");
+    printf("ldboot <filename>   load boot area from <filename>\n");
+    printf("clrdir              clear directory\n");
+    printf("q                   quit\n");
+    printf("cmd > ");
+    fflush(stdout);
+    if (fgets(line, LINE_SIZE, stdin) == NULL) {
+      printf("\n");
+      break;
+    }
+    n = tokenize(line, tokens, MAX_TOKENS);
+    if (n == 0) {
+      continue;
+    }
+    cmd = lookupCmd(tokens[0]);
+    printf("\n");
+    if (cmd == NULL) {
+      printf("error: unknown command '%s'\n", tokens[0]);
+      continue;
+    }
+    if (n < cmd->minArgc) {
+      printf("error: too few arguments for command '%s'\n", tokens[0]);
+      continue;
+    }
+    (*cmd->func)(n, tokens);
+  }
   serialClose();
   return 0;
 }
