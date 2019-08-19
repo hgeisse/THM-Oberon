68b7fa97da45910c7e60ec257f2658431084973d
Hellwig Geisse <hellwig.geisse@mni.thm.de>
2019-08-19 18:32:34 +0200

    add another two converters

---
+++ tools/dos2oberon/dos2oberon.c
@@ -0,0 +1,44 @@
+/*
+ * dos2oberon.c -- convert DOS line endings to Oberon line endings
+ */
+
+
+#include <stdio.h>
+
+
+int main(int argc, char *argv[]) {
+  FILE *in, *out;
+  int c;
+
+  if (argc != 3) {
+    printf("usage: %s <input file> <output file>\n", argv[0]);
+    return 1;
+  }
+  in = fopen(argv[1], "r");
+  if (in == NULL) {
+    printf("error: cannot open input file '%s'\n", argv[1]);
+    return 1;
+  }
+  out = fopen(argv[2], "w");
+  if (out == NULL) {
+    printf("error: cannot open output file '%s'\n", argv[2]);
+    return 1;
+  }
+  while (1) {
+    c = fgetc(in);
+    if (c == EOF) {
+      break;
+    }
+    if (c == '\r') {
+      c = fgetc(in);
+      if (c != EOF && c != '\n') {
+        ungetc(c, in);
+      }
+      c = '\r';
+    }
+    fputc(c, out);
+  }
+  fclose(in);
+  fclose(out);
+  return 0;
+}
---
+++ tools/oberon2dos/oberon2dos.c
@@ -0,0 +1,41 @@
+/*
+ * oberon2dos.c -- convert Oberon line endings to DOS line endings
+ */
+
+
+#include <stdio.h>
+
+
+int main(int argc, char *argv[]) {
+  FILE *in, *out;
+  int c;
+
+  if (argc != 3) {
+    printf("usage: %s <input file> <output file>\n", argv[0]);
+    return 1;
+  }
+  in = fopen(argv[1], "r");
+  if (in == NULL) {
+    printf("error: cannot open input file '%s'\n", argv[1]);
+    return 1;
+  }
+  out = fopen(argv[2], "w");
+  if (out == NULL) {
+    printf("error: cannot open output file '%s'\n", argv[2]);
+    return 1;
+  }
+  while (1) {
+    c = fgetc(in);
+    if (c == EOF) {
+      break;
+    }
+    if (c == '\r') {
+      fputc(c, out);
+      c = '\n';
+    }
+    fputc(c, out);
+  }
+  fclose(in);
+  fclose(out);
+  return 0;
+}
