b397f4862e65808e2ed455a091ad11c8aa37f109
Hellwig Geisse <hellwig.geisse@mni.thm.de>
2021-01-21 01:52:20 +0100

    add fake floating-point modules

---
+++ fpga-RISC5/v1/sim/src/cpu/fpmul.v
@@ -0,0 +1,22 @@
+//
+// fpmul.v -- floating-point multiply
+//
+
+
+`timescale 1ns / 1ps
+`default_nettype none
+
+
+module fpmul(clk, run, stall,
+             x, y, z);
+    input clk;
+    input run;
+    output stall;
+    input [31:0] x;
+    input [31:0] y;
+    output [31:0] z;
+
+  // fake return = 3.1415927410e-01
+  assign z[31:0] = 32'h3EA0D97C;
+
+endmodule
