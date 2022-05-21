d4c9c80e5b1f0bae81f1a96c8b3ba8d27988cfde
Hellwig Geisse <hellwig.geisse@mni.thm.de>
2022-05-22 00:24:47 +0200

    add assembler test generator

---
+++ tools/asm/RISC5/test/gen1.c
@@ -0,0 +1,163 @@
+/*
+ * generate bit patterns for assembler test
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int F0(void) {
+  int n;
+  unsigned int word;
+
+  n = 0;
+  /* standard register/register operations */
+  for (word = 0; word < (1 << 16); word++) {
+    if ((word & 0x00F0) == 0x0000 &&
+        (word & 0x0F00) != 0x0000) {
+      /* skip redundant MOV codings */
+      continue;
+    }
+    printf("0x%08X\n", (0x0 << 28) |
+                       ((word & 0xFFF0) << 12) |
+                       ((word & 0x000F) <<  0));
+    n++;
+  }
+  return n;
+}
+
+
+int F1(void) {
+  int n;
+  unsigned int word;
+
+  n = 0;
+  /* standard register/immediate operations, extension bit = 0 */
+  for (word = 0; word < (1 << 16); word++) {
+    if ((word & 0x00F0) == 0x0000 &&
+        (word & 0x0F00) != 0x0000) {
+      /* skip redundant MOV codings */
+      continue;
+    }
+    printf("0x%08X\n", (0x4 << 28) |
+                       ((word & 0xFFF0) << 12) |
+                       ((word & 0x000F) <<  0));
+    n++;
+  }
+  /* standard register/immediate operations, extension bit = 1 */
+  for (word = 0; word < (1 << 16); word++) {
+    if ((word & 0x00F0) == 0x0000 &&
+        (word & 0x0F00) != 0x0000) {
+      /* skip redundant MOV codings */
+      continue;
+    }
+    printf("0x%08X\n", (0x5 << 28) |
+                       ((word & 0xFFF0) << 12) |
+                       ((word & 0x000F) <<  0));
+    n++;
+  }
+  return n;
+}
+
+
+int F2(void) {
+  int n;
+  unsigned int word;
+
+  n = 0;
+  /* load word */
+  for (word = 0; word < (1 << 16); word++) {
+    printf("0x%08X\n", (0x8 << 28) |
+                       ((word & 0xFFF0) << 12) |
+                       ((word & 0x000F) <<  0));
+    n++;
+  }
+  /* load byte */
+  for (word = 0; word < (1 << 16); word++) {
+    printf("0x%08X\n", (0x9 << 28) |
+                       ((word & 0xFFF0) << 12) |
+                       ((word & 0x000F) <<  0));
+    n++;
+  }
+  /* store word */
+  for (word = 0; word < (1 << 16); word++) {
+    printf("0x%08X\n", (0xA << 28) |
+                       ((word & 0xFFF0) << 12) |
+                       ((word & 0x000F) <<  0));
+    n++;
+  }
+  /* store byte */
+  for (word = 0; word < (1 << 16); word++) {
+    printf("0x%08X\n", (0xB << 28) |
+                       ((word & 0xFFF0) << 12) |
+                       ((word & 0x000F) <<  0));
+    n++;
+  }
+  return n;
+}
+
+
+int F3(void) {
+  int n;
+  unsigned int word;
+
+  n = 0;
+  /* branch with register */
+  for (word = 0; word < (1 << 8); word++) {
+    printf("0x%08X\n", (0xC << 28) |
+                       ((word & 0xF0) << 20) |
+                       ((word & 0x0F) <<  0));
+    n++;
+  }
+  /* call with register */
+  for (word = 0; word < (1 << 8); word++) {
+    printf("0x%08X\n", (0xD << 28) |
+                       ((word & 0xF0) << 20) |
+                       ((word & 0x0F) <<  0));
+    n++;
+  }
+  /* branch with offset */
+  for (word = 0; word < (1 << 12); word++) {
+    printf("0x%08X\n", (0xE << 28) |
+                       ((word & 0xF00) << 16) |
+                       ((word & 0x0F0) << 14) |
+                       ((word & 0x00F) <<  0));
+    n++;
+  }
+  /* call with offset */
+  for (word = 0; word < (1 << 12); word++) {
+    printf("0x%08X\n", (0xF << 28) |
+                       ((word & 0xF00) << 16) |
+                       ((word & 0x0F0) << 14) |
+                       ((word & 0x00F) <<  0));
+    n++;
+  }
+  /* return from interrupt */
+  printf("0x%08X\n", 0xC7000010);
+  n++;
+  /* clear interrupt enable */
+  printf("0x%08X\n", 0xCF000020);
+  n++;
+  /* set interrupt enable */
+  printf("0x%08X\n", 0xCF000021);
+  n++;
+  return n;
+}
+
+
+int main(int argc, char *argv[]) {
+  int n0, n1, n2, n3;
+
+  n0 = F0();
+  fprintf(stderr, "number of F0 patterns = %d\n", n0);
+  n1 = F1();
+  fprintf(stderr, "number of F1 patterns = %d\n", n1);
+  n2 = F2();
+  fprintf(stderr, "number of F2 patterns = %d\n", n2);
+  n3 = F3();
+  fprintf(stderr, "number of F3 patterns = %d\n", n3);
+  fprintf(stderr, "total number of patterns = %d\n", n0 + n1 + n2 + n3);
+  return 0;
+}
---
+++ tools/asm/RISC5/test/gen2.c
@@ -0,0 +1,259 @@
+/*
+ * generate assembler source from bit patterns
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#define LINE_SIZE	200
+
+#define SIGN_EXT_20(x)	((x) & 0x00080000 ? (x) | 0xFFF00000 : (x))
+
+#define ADDR_MASK	0x00FFFFFF
+
+
+typedef unsigned int Word;
+
+
+/**************************************************************/
+
+/*
+ * disassembler
+ */
+
+
+static char instrBuffer[100];
+
+
+static char *regOps[16] = {
+  /* 0x00 */  "MOV", "LSL", "ASR", "ROR",
+  /* 0x04 */  "AND", "ANN", "IOR", "XOR",
+  /* 0x08 */  "ADD", "SUB", "MUL", "DIV",
+  /* 0x0C */  "FAD", "FSB", "FML", "FDV",
+};
+
+
+static void disasmF0(Word instr) {
+  int a, b, op, c;
+
+  a = (instr >> 24) & 0x0F;
+  b = (instr >> 20) & 0x0F;
+  op = (instr >> 16) & 0x0F;
+  c = instr & 0x0F;
+  if (op == 0) {
+    /* MOV */
+    if (((instr >> 29) & 1) == 0) {
+      /* u = 0: move from any general register */
+      sprintf(instrBuffer, "%-7s R%d,R%d", regOps[op], a, c);
+    } else {
+      /* u = 1: move from special register */
+      if (((instr >> 28) & 1) == 0) {
+        /* v = 0: get H register */
+        sprintf(instrBuffer, "%-7s R%d", "GETH", a);
+      } else {
+        /* v = 1: get flag values and CPU ID */
+        sprintf(instrBuffer, "%-7s R%d", "GETF", a);
+      }
+    }
+  } else {
+    /* any operation other than MOV */
+    sprintf(instrBuffer, "%-7s R%d,R%d,R%d", regOps[op], a, b, c);
+    if ((op == 8 || op == 9) && ((instr >> 29) & 1) != 0) {
+      /* ADD/SUB with u = 1: add/subtract with carry/borrow */
+      instrBuffer[3] = (op == 8) ? 'C' : 'B';
+    } else
+    if ((op == 10 || op == 11) && ((instr >> 29) & 1) != 0) {
+      /* MUL/DIV with u = 1: unsigned mul/div */
+      instrBuffer[3] = 'U';
+    } else
+    if (op == 12 && ((instr >> 29) & 1) != 0 && ((instr >> 28) & 1) == 0) {
+      /* FAD with u = 1, v = 0: FLT (INTEGER -> REAL) */
+      sprintf(instrBuffer, "%-7s R%d,R%d", "FLT", a, b);
+    } else
+    if (op == 12 && ((instr >> 29) & 1) == 0 && ((instr >> 28) & 1) != 0) {
+      /* FAD with u = 0, v = 1: FLR (REAL -> INTEGER) */
+      sprintf(instrBuffer, "%-7s R%d,R%d", "FLR", a, b);
+    }
+  }
+}
+
+
+static void disasmF1(Word instr) {
+  int a, b, op, im;
+
+  a = (instr >> 24) & 0x0F;
+  b = (instr >> 20) & 0x0F;
+  op = (instr >> 16) & 0x0F;
+  im = instr & 0xFFFF;
+  if ((instr >> 28) & 1) {
+    /* v = 1: fill upper 16 bits with 1 */
+    im |= 0xFFFF0000;
+  }
+  if (op == 0) {
+    /* MOV */
+    if (((instr >> 29) & 1) == 0) {
+      /* u = 0: use immediate value as is */
+      sprintf(instrBuffer, "%-7s R%d,0x%08X", regOps[op], a, im);
+    } else {
+      /* u = 1: shift immediate value to upper 16 bits */
+      sprintf(instrBuffer, "%-7s R%d,0x%08X", regOps[op], a, im);
+      instrBuffer[3] = 'H';
+    }
+  } else {
+    /* any operation other than MOV */
+    sprintf(instrBuffer, "%-7s R%d,R%d,0x%08X", regOps[op], a, b, im);
+    if ((op == 8 || op == 9) && ((instr >> 29) & 1) != 0) {
+      /* ADD/SUB with u = 1: add/subtract with carry/borrow */
+      instrBuffer[3] = (op == 8) ? 'C' : 'B';
+    } else
+    if ((op == 10 || op == 11) && ((instr >> 29) & 1) != 0) {
+      /* MUL/DIV with u = 1: unsigned mul/div */
+      instrBuffer[3] = 'U';
+    }
+  }
+}
+
+
+static void disasmF2(Word instr) {
+  char *opName;
+  int a, b;
+  int offset;
+
+  if (((instr >> 29) & 1) == 0) {
+    /* u = 0: load */
+    if (((instr >> 28) & 1) == 0) {
+      /* v = 0: word */
+      opName = "LDW";
+    } else {
+      /* v = 1: byte */
+      opName = "LDB";
+    }
+  } else {
+    /* u = 1: store */
+    if (((instr >> 28) & 1) == 0) {
+      /* v = 0: word */
+      opName = "STW";
+    } else {
+      /* v = 1: byte */
+      opName = "STB";
+    }
+  }
+  a = (instr >> 24) & 0x0F;
+  b = (instr >> 20) & 0x0F;
+  offset = SIGN_EXT_20(instr & 0x000FFFFF);
+  sprintf(instrBuffer, "%-7s R%d,R%d,%s0x%05X",
+          opName, a, b,
+          offset < 0 ? "-" : "+",
+          offset < 0 ? -offset : offset);
+}
+
+
+static char *condName[16] = {
+  /* 0x00 */  "MI", "EQ", "CS", "VS", "LS", "LT", "LE", "",
+  /* 0x08 */  "PL", "NE", "CC", "VC", "HI", "GE", "GT", "NVR",
+};
+
+
+static void disasmF3(Word instr, Word locus) {
+  char *cond;
+  int c;
+  int offset;
+  Word target;
+
+  cond = condName[(instr >> 24) & 0x0F];
+  if (((instr >> 29) & 1) == 0) {
+    /* u = 0: branch target is in register */
+    c = instr & 0x0F;
+    if (((instr >> 28) & 1) == 0) {
+      /* v = 0: branch or interrupt handling */
+      switch ((instr >> 4) & 3) {
+        case 0:
+          /* branch */
+          sprintf(instrBuffer, "B%-6s R%d", cond, c);
+          break;
+        case 1:
+          /* return from interrupt */
+          sprintf(instrBuffer, "RTI");
+          break;
+        case 2:
+          /* clear/set interrupt enable */
+          if ((instr & 1) == 0) {
+            sprintf(instrBuffer, "CLI");
+          } else {
+            sprintf(instrBuffer, "STI");
+          }
+          break;
+        case 3:
+          /* undefined */
+          sprintf(instrBuffer, "<undefined>");
+          break;
+      }
+    } else {
+      /* v = 1: call */
+      sprintf(instrBuffer, "C%-6s R%d", cond, c);
+    }
+  } else {
+    /* u = 1: branch target is pc + 1 + offset */
+    offset = instr & 0x003FFFFF;
+    target = (((locus >> 2) + 1 + offset) << 2) & ADDR_MASK;
+    if (((instr >> 28) & 1) == 0) {
+      /* v = 0: branch */
+      sprintf(instrBuffer, "B%-6s 0x%08X", cond, target);
+    } else {
+      /* v = 1: call */
+      sprintf(instrBuffer, "C%-6s 0x%08X", cond, target);
+    }
+  }
+}
+
+
+char *disasm(Word instr, Word locus) {
+  switch ((instr >> 30) & 3) {
+    case 0:
+      disasmF0(instr);
+      break;
+    case 1:
+      disasmF1(instr);
+      break;
+    case 2:
+      disasmF2(instr);
+      break;
+    case 3:
+      disasmF3(instr, locus);
+      break;
+  }
+  return instrBuffer;
+}
+
+
+/**************************************************************/
+
+
+int main(int argc, char *argv[]) {
+  FILE *bitpat;
+  char line[LINE_SIZE];
+  Word instr, locus;
+  char *instrText;
+
+  if (argc != 2) {
+    printf("usage: %s <input file>\n", argv[0]);
+    return 1;
+  }
+  bitpat = fopen(argv[1], "r");
+  if (bitpat == NULL) {
+    printf("error: cannot open input file '%s'\n", argv[1]);
+    return 1;
+  }
+  locus = 0;
+  while (fgets(line, LINE_SIZE, bitpat) != NULL) {
+    instr = strtoul(line, NULL, 0);
+    instrText = disasm(instr, locus);
+    printf("\t%s\t\t// 0x%06X: 0x%08X\n", instrText, locus, instr);
+    locus += 4;
+  }
+  fclose(bitpat);
+  return 0;
+}
