3e007dbf659bd5d4586a3da16953f28b907d2225
Hellwig Geisse <hellwig.geisse@mni.thm.de>
2022-06-10 09:24:46 +0200

    reorganize experiment 3 into a series of parts

--- fpga-RISC5/v2/exp-03/v0/ramctrl/ram.v
+++ fpga-RISC5/v2/exp-03/v0/ramctrl/ram.v
@@ -4,6 +4,12 @@
 //
 
 
+//
+// NOTE: This RAM is specifically modified: it cannot store anything.
+//       It computes the data output from the address input.
+//
+
+
 `timescale 1ns/1ps
 `default_nettype none
 
@@ -26,14 +32,14 @@ module ram(clk, rst,
 
   reg we_buf;
   reg [21:0] addr_buf;
-  reg [31:0] mem[0:4194303];
   reg [3:0] latency_cnt;
   wire xfer_start;
   reg [3:0] address_cnt;
   wire xfer_stop;
   reg rd_ack;
   wire [2:0] ram_addr_lo;
-  wire [21:0] ram_addr;
+  wire [23:0] ram_addr;
+  wire [31:0] ram_data;
 
   always @(posedge clk) begin
     if (rst) begin
@@ -86,62 +92,18 @@ module ram(clk, rst,
   assign ack = we_buf ? ~xfer_stop : rd_ack;
 
   assign ram_addr_lo[2:0] = addr_buf[2:0] + address_cnt[2:0];
-  assign ram_addr[21:0] = { addr_buf[21:3], ram_addr_lo[2:0] };
+  assign ram_addr[23:0] = { addr_buf[21:3], ram_addr_lo[2:0], 2'b00 };
+
+  assign ram_data[31:0] =
+    { ~ram_addr[18:14], ram_addr[5:2], ~ram_addr[9:7], ram_addr[13:10],
+      ram_addr[8:6], ~ram_addr[13:10], ram_addr[23:19], ~ram_addr[5:2] };
 
   always @(posedge clk) begin
     if (~xfer_stop) begin
-      if (we_buf) begin
-        mem[ram_addr] <= data_in;
-      end else begin
-        data_out <= mem[ram_addr];
+      if (~we_buf) begin
+        data_out <= ram_data;
       end
     end
-  end
-
-  initial begin
-    mem[22'h351AFC] = 32'h1234FFFF;
-    mem[22'h351AFD] = 32'h1234FFFF;
-    mem[22'h351AFE] = 32'h1234FFFF;
-    mem[22'h351AFF] = 32'h1234FFFF;
-
-    mem[22'h351B00] = 32'h12340101;
-    mem[22'h351B01] = 32'h12341101;
-    mem[22'h351B02] = 32'h12342101;
-    mem[22'h351B03] = 32'h12343101;
-    mem[22'h351B04] = 32'h12344101;
-    mem[22'h351B05] = 32'h12345101;
-    mem[22'h351B06] = 32'h12346101;
-    mem[22'h351B07] = 32'h12347101;
-    mem[22'h351B08] = 32'h12348101;
-    mem[22'h351B09] = 32'h12349101;
-    mem[22'h351B0A] = 32'h1234A101;
-    mem[22'h351B0B] = 32'h1234B101;
-    mem[22'h351B0C] = 32'h1234C101;
-    mem[22'h351B0D] = 32'h1234D101;
-    mem[22'h351B0E] = 32'h1234E101;
-    mem[22'h351B0F] = 32'h1234F101;
-
-    mem[22'h351B10] = 32'h12340201;
-    mem[22'h351B11] = 32'h12341201;
-    mem[22'h351B12] = 32'h12342201;
-    mem[22'h351B13] = 32'h12343201;
-    mem[22'h351B14] = 32'h12344201;
-    mem[22'h351B15] = 32'h12345201;
-    mem[22'h351B16] = 32'h12346201;
-    mem[22'h351B17] = 32'h12347201;
-    mem[22'h351B18] = 32'h12348201;
-    mem[22'h351B19] = 32'h12349201;
-    mem[22'h351B1A] = 32'h1234A201;
-    mem[22'h351B1B] = 32'h1234B201;
-    mem[22'h351B1C] = 32'h1234C201;
-    mem[22'h351B1D] = 32'h1234D201;
-    mem[22'h351B1E] = 32'h1234E201;
-    mem[22'h351B1F] = 32'h1234F201;
-
-    mem[22'h351B20] = 32'h1234FFFF;
-    mem[22'h351B21] = 32'h1234FFFF;
-    mem[22'h351B22] = 32'h1234FFFF;
-    mem[22'h351B23] = 32'h1234FFFF;
   end
 
 endmodule
