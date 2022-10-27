48d67eb84b38aecfa5bdce8c58e9881b807ddc92
Hellwig Geisse <hellwig.geisse@mni.thm.de>
2022-10-27 12:17:10 +0200

    add interrupt capability for the timer

--- fpga-RISC5/v1/sim/src/tmr/tmr.v
+++ fpga-RISC5/v1/sim/src/tmr/tmr.v
@@ -7,16 +7,18 @@
 `default_nettype none
 
 
-module tmr(clk, rst, stb, data_out, ack);
+module tmr(clk, rst, stb, data_out, ack, irq);
     input clk;
     input rst;
     input stb;
     output [31:0] data_out;
     output ack;
+    output reg irq;
 
   wire millisec;
   reg [15:0] cnt0;
   reg [31:0] cnt1;
+  reg tick;
 
   assign millisec = (cnt0[15:0] == 16'd49999);
 
@@ -35,9 +37,27 @@ module tmr(clk, rst, stb, data_out, ack);
   always @(posedge clk) begin
     if (rst) begin
       cnt1[31:0] <= 32'd0;
+      tick <= 1'b0;
     end else begin
       if (millisec) begin
         cnt1[31:0] <= cnt1[31:0] + 32'd1;
+        tick <= 1'b1;
+      end else begin
+        tick <= 1'b0;
+      end
+    end
+  end
+
+  always @(posedge clk) begin
+    if (rst) begin
+      irq <= 1'b0;
+    end else begin
+      if (tick) begin
+        irq <= 1'b1;
+      end else begin
+        if (stb) begin
+          irq <= 1'b0;
+        end
       end
     end
   end
